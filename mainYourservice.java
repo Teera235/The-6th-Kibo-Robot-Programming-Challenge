915d045c6452515c9f25cc119e455fb2c05a5664
DocTeeraThxp <106339662+Teera235@users.noreply.github.com>
2025-06-14 04:04:16 +0700

    Update mainYourservice.java

--- mainYourservice.java
+++ mainYourservice.java
@@ -1,108 +1,301 @@
 package jp.jaxa.iss.kibo.rpc.sampleapk;
 
+
+import android.util.Log;
+
+
 import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
+import java.util.ArrayList;
+import java.util.List;
+
+
 import gov.nasa.arc.astrobee.types.Point;
 import gov.nasa.arc.astrobee.types.Quaternion;
+
+
+import org.opencv.aruco.Aruco;
+import org.opencv.aruco.Dictionary;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
-import java.util.HashMap;
-import java.util.Map;
+import org.opencv.imgproc.Imgproc;
+
+
+import java.util.Dictionary;
+
+
+/**
+
+ * Class meant to handle commands from the Ground Data System and execute them in Astrobee.
+
+ */
+
 
 public class YourService extends KiboRpcService {
-    private final Point[] areaPoints = {
-        new Point(10.95, -10.58, 5.195),
-        new Point(10.925, -8.875, 3.76203),
-        new Point(10.925, -7.925, 3.76093),
-        new Point(9.866984, -6.8525, 4.945)
+    private final String TAG = this.getClass().getSimpleName();
+
+// Template file name
+    private final String[] TEMPLATE_FILE_NAME = {
+            "coin.png",
+            "compass.png",
+            "coral.png",
+            "crystal.png",
+            "emerald.png",
+            "fossil.png",
+            "key.png",
+            "letter.png",
+            "shell.png",
+            "treasure_box.png"
     };
-    private final Quaternion areaQuaternion = new Quaternion(0f, 0f, -0.707f, 0.707f);
-
-    private final Point[] oasisPoints = {
-        new Point(10.925, -9.85, 4.695),
-        new Point(11.175, -8.975, 5.195),
-        new Point(10.7, -7.925, 5.195),
-        new Point(11.175, -6.875, 4.685)
+// Template name
+    private final String[] TEMPLATE_NAME = {
+            "coin",
+            "compass",
+            "coral",
+            "crystal",
+            "emerald",
+            "fossil",
+            "key",
+            "letter",
+            "shell",
+            "treasure_box"
     };
-
-    private final Map<Integer, ItemRecognitionResult> areaItems = new HashMap<>();
-    private String targetItemName = null;
-    private int targetArea = -1;
-
     @Override
-    protected void runPlan1() {
+    protected void runPlan1(){
+
+        Log.i(TAG, "start mission");
+
+
+        // The mission starts.
         api.startMission();
 
-        for (int i = 0; i < 4; i++) {
-            if (i > 0) {
-                moveThroughOasis(i - 1);
-            }
-            api.moveTo(areaPoints[i], areaQuaternion, false);
-            Mat image = api.getMatNavCam();
-            ItemRecognitionResult result = recognizeItems(image);
-            areaItems.put(i + 1, result);
-            api.setAreaInfo(i + 1, result.landmarkName, result.landmarkQuantity);
-            if (result.treasureName != null && targetItemName == null) {
-                targetItemName = result.treasureName;
-                targetArea = i + 1;
+        // Move to a point.
+        Point point = new Point(10.9d, -9.92284d, 5.195d);
+        Quaternion quaternion = new Quaternion(0f, 0f, -0.707f, 0.707f);
+        api.moveTo(point, quaternion, false);
+
+        // Get a camera image.
+        Mat image = api.getMatNavCam();
+
+
+        // Save the image.
+        api.saveMatImage(image, "file.png");
+
+
+        /* ******************************************************************************** */
+        /* Write your code to recognize the type and number of landmark items in each area! */
+        /* If there is a treasure item, remember it. */
+        /* ******************************************************************************** */
+
+
+        // Detect AR
+
+        Dictionary dictionary = Aruco.getPredefinedDictionary(Aruco.DICT_5X5_250);
+        List<Mat> corners = new ArrayList<>();
+        Mat markerIds = new Mat();
+        Aruco.detectMarkers(image, dictionary, corners, markerIds);
+
+
+        // Get camera matrix
+        Mat cameraMatrix = new Mat(3, 3, CvType.CV_64F);
+        cameraMatrix.put(0, 0, api.getNavCamIntrinsics()[0]);
+        // Get lens distortion parameters
+        Mat cameraCoefficients = new Mat(1, 5, CvType.CV_64F);
+        cameraCoefficients.put(0, 0, api.getNavCamIntrinsics()[1]);
+        cameraCoefficients.convertTo(cameraCoefficients, CvType.CV_64F);
+
+        // Undistort image
+        Mat undistortImg = new Mat();
+        Calib3d.undistort(image, undistortImg, cameraMatrix, cameraCoefficients);
+
+
+        // Pattern matching
+        // Load template images
+        Mat[] templates = new Mat[TEMPLATE_FILE_NAME.length];
+        for (int i = 0; i < TEMPLATE_FILE_NAME.length; i++) {
+            try {
+                // Open the template image file in Bitmap from the file name and convert to Mat
+                InputStream inputStream = getAssets().open(TEMPLATE_FILE_NAME[i]);
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                Mat mat = new Mat();
+                Utils.bitmapToMat(bitmap, mat);
+
+
+                // Convert to grayscale
+                Imgproc.cvtColor(mat, mat, Imgproc.COLOR_BGR2GRAY);
+
+
+                // Assign to an array of templates
+                templates[i] = mat;
+
+                inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
+        // Number of matches for each template
+        int templateMatchCnt[] = new int[templates.length];
 
-        moveThroughOasis(3);
-        Point astronautPoint = new Point(11.143, -6.7607, 4.9654);
-        Quaternion astronautQuaternion = new Quaternion(0f, 0f, 0.707f, 0.707f);
-        api.moveTo(astronautPoint, astronautQuaternion, false);
+
+        //Get the number of template matches
+        for (int tempNum = 0; tempNum < templates.length; tempNum++) {
+            // Number of matches
+            int matchCnt = 0;
+
+
+            // Coordinates of the matched location
+            List<org.opencv.core.Point> matches = new ArrayList<>();
+
+
+            // Loading template image and target image
+            Mat template = templates[tempNum].clone();
+            Mat targetImg = undistortImg.clone();
+            // Pattern matching
+            int widthMin = 20; //[px]
+            int widthMax = 100; //[px]
+            int changeWidth = 5; //[px]
+            int changeAngle = 45; //[px]
+
+
+            for (int size = widthMin; size <= widthMax; size += changeWidth) {
+                for (int angle = 0; angle < 360; angle += changeAngle) {
+                    Mat resizedTemp = resizeImg(template, size);
+                    Mat rotResizedTemp = rotImg(resizedTemp, angle);
+
+
+                    Mat result = new Mat();
+                    Imgproc.matchTemplate(targetImg, rotResizedTemp, result, Imgproc.TM_CCOEFF_NORMED);
+
+
+                    // Get coordinates with similarity greater than or equal to the threshold
+                    double threshold = 0.7;
+                    Core.MinMaxLocResult mmlr = Core.minMaxLoc(result);
+                    double maxVal = mmlr.maxVal;
+                    if (maxVal >= threshold) {
+                        // Extract only results greater than or equal to the threshold
+                        Mat thresholdedResult = new Mat();
+                        Imgproc.threshold(result, thresholdedResult, threshold, 1.0, Imgproc.THRESH_TOZERO);
+
+                        // Get coordinates of the matched location
+                        for (int y = 0; y < thresholdedResult.rows(); y++) {
+                            for (int x = 0; x < thresholdedResult.cols(); x++) {
+                                if (thresholdedResult.get(y, x)[0] > 0) {
+                                    matches.add(new org.opencv.core.Point(x, y));
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
+            // Avoid detecting the same location multiple times
+            List<org.opencv.core.Point> filteredMatches = removeDuplicates(matches);
+            matchCnt += filteredMatches.size();
+
+            // Number of matches for each template
+            templateMatchCnt[tempNum] = matchCnt;
+        }
+
+
+        // When you recognize landmark items, let's set the type and number.
+        int mostMatchTemplateNum = getMaxIndex(templateMatchCnt);
+        api.setAreaInfo( 1 , TEMPLATE_NAME[mostMatchTemplateNum], templateMatchCnt[mostMatchTemplateNum]);
+
+        /* **************************************************** */
+        /* Let's move to each area and recognize the items. */
+        /* **************************************************** */
+
+
+        // When you move to the front of the astronaut, report the rounding completion.
+        point = new Point(11.143d, -6.7607d, 4.9654d);
+        quaternion = new Quaternion(0f, 0f, 0.707f, 0.707f);
+        api.moveTo(point, quaternion, false);
         api.reportRoundingCompletion();
 
-        Mat astronautImage = api.getMatNavCam();
-        String astronautTarget = recognizeTargetItem(astronautImage);
+
+        /* ********************************************************** */
+        /* Write your code to recognize which target item the astronaut has. */
+        /* ********************************************************** */
+
+
+        // Let's notify the astronaut when you recognize it.
         api.notifyRecognitionItem();
 
-        if (astronautTarget != null) {
-            targetItemName = astronautTarget;
-            targetArea = findTargetArea(targetItemName);
-        }
+        /* ******************************************************************************************************* */
+        /* Write your code to move Astrobee to the location of the target item (what the astronaut is looking for) */
+        /* ******************************************************************************************************* */
 
-        if (targetArea > 0) {
-            Point targetPoint = areaPoints[targetArea - 1];
-            api.moveTo(targetPoint, areaQuaternion, false);
-            api.takeTargetItemSnapshot();
-        }
+
+        // Take a snapshot of the target item.
+        api.takeTargetItemSnapshot();
     }
 
-    private void moveThroughOasis(int index) {
-        api.moveTo(oasisPoints[index], areaQuaternion, false);
+
+    @Override
+    protected void runPlan2(){
+        // write your plan 2 here.
+    }
+    @Override
+    protected void runPlan3(){
+        // write your plan 3 here.
     }
 
-    private ItemRecognitionResult recognizeItems(Mat image) {
-        ItemRecognitionResult result = new ItemRecognitionResult();
-        result.landmarkName = "shell";
-        result.landmarkQuantity = 1;
-        if (Math.random() > 0.3) {
-            result.treasureName = "diamond";
-        }
-        return result;
+    // You can add your method.
+    private String yourMethod(){
+        return "your method";
     }
+    // Resize image
+    private Mat resizeImg(Mat img, int width) {
+        int height = (int) (img.rows() * ((double) width / img.cols()));
+        Mat resizedImg = new Mat();
+        Imgproc.resize(img, resizedImg, new Size(width, height));
 
-    private String recognizeTargetItem(Mat image) {
-        return "diamond";
+        return resizedImg;
     }
+    // Rotate image
+    private Mat rotImg(Mat img, int angle) {
+        org.opencv.core.Point center = new org.opencv.core.Point(img.cols() / 2.0, img.rows() / 2.0);
+        Mat rotatedMat = Imgproc.getRotationMatrix2D(center, angle, 1.0);
+        Mat rotatedImg = new Mat();
+        Imgproc.warpAffine(img, rotatedImg, rotatedMat, img.size());
 
-    private int findTargetArea(String targetItemName) {
-        for (Map.Entry<Integer, ItemRecognitionResult> entry : areaItems.entrySet()) {
-            if (targetItemName.equals(entry.getValue().treasureName)) {
-                return entry.getKey();
+        return rotatedImg;
+    }
+    // remove multiple detections
+    private List<org.opencv.core.Point> removeDuplicates(List<org.opencv.core.Point> points) {
+        double length = 10; // Within 10 px
+        List<org.opencv.core.Point> filteredList = new ArrayList<>();
+        for (org.opencv.core.Point point : points) {
+            boolean isInclude = false;
+            for (org.opencv.core.Point checkPoint : filteredList) {
+                double distance = calculateDistance(point, checkPoint);
+                if (distance <= length) {
+                    isInclude = true;
+                    break;
+                }
+            }
+            if (!isInclude) {
+                filteredList.add(point);
             }
         }
-        return -1;
+        return filteredList;
     }
-
-    private static class ItemRecognitionResult {
-        String landmarkName;
-        int landmarkQuantity;
-        String treasureName;
+    // Find the distance between two points
+    private double calculateDistance(org.opencv.core.Point p1, org.opencv.core.Point p2) {
+        double dx = p1.x - p2.x;
+        double dy = p1.y - p2.y;
+        return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
     }
-
-    @Override
-    protected void runPlan2() {}
-    @Override
-    protected void runPlan3() {}
+    // Get the maximum value of an array
+    private int getMaxIndex(int[] array) {
+        int max = 0;
+        int maxIndex = 0;
+        // Find the index of the element with the largest value
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+                maxIndex = i;
+            }
+        }
+        return maxIndex;
+    }
 }
